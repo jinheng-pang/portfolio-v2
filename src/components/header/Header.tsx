880288128f4a9c0adfb61fa7865227da989edf9c
jinhengpang <pangjinheng58@gmail.com>
2025-10-26 00:35:14 +1100

    Populate header links - mail - github - linkedin

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './header.css'
+import LinkItem from "../link/LinkItem";
+import SvgMailIcon from "@/svgs/MailIcon";
+import SvgGithubIcon from "@/svgs/GithubIcon";
+import SvgLinkedinIcon from "@/svgs/LinkedinIcon";
 
 const Header = () => {
   return <header className="header neumorphic">
@@ -7,7 +11,11 @@ const Header = () => {
       <h1 className="header-title">Jin Heng Pang</h1>
       <p className="header-subtitle">Software Engineer</p>
     </div>
-    <div className="header-links"></div>
+    <ul className="header-links">
+      <LinkItem href="hi" Icon={SvgMailIcon} label="Contact"  />
+      <LinkItem href="https://github.com/jinheng-pang" Icon={SvgGithubIcon} label="Github"  />
+      <LinkItem href="https://www.linkedin.com/in/jin-heng-pang/" Icon={SvgLinkedinIcon} label="LinkedIn"  />
+    </ul>
   </header>;
 };
 
