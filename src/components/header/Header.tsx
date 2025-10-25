b600dab0da572fb07ad002029f1252d9fd01f805
jinhengpang <pangjinheng58@gmail.com>
2025-10-25 21:32:51 +1100

    Style header titles - main font for this site is Roboto Mono - utilise clamp function for responsive design

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,10 @@ import './header.css'
 
 const Header = () => {
   return <header className="header neumorphic">
-    <h1 className="header-title">Jin Heng Pang</h1>
-    <p className="header-subtitle">Software Engineer</p>
+    <div className="header-titles">
+      <h1 className="header-title">Jin Heng Pang</h1>
+      <p className="header-subtitle">Software Engineer</p>
+    </div>
     <div className="header-links"></div>
   </header>;
 };
