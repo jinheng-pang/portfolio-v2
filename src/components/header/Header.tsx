6dda8efaf7a3e45d105ce2554c58d3d751657672
jinhengpang <pangjinheng58@gmail.com>
2025-10-25 21:12:14 +1100

    Initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+  return <header className="header neumorphic">
+    <h1 className="header-title">Jin Heng Pang</h1>
+    <p className="header-subtitle">Software Engineer</p>
+    <div className="header-links"></div>
+  </header>;
+};
+
+export default Header;
