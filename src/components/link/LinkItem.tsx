8c864a887ba671431e2cd20115a218b45ec9b16d
jinhengpang <pangjinheng58@gmail.com>
2025-10-26 00:32:01 +1100

    Create link item

---
+++ src/components/link/LinkItem.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface LinkItemProps {
+  href: string;
+  label: string;
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+const LinkItem: React.FC<LinkItemProps> = ({ href, label, Icon }) => {
+  return (
+    <a
+      href={href}
+      className="header-link neumorphic-hover"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon className="header-svg" />
+    </a>
+  );
+};
+
+export default LinkItem;
