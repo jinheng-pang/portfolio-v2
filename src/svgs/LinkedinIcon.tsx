4b8e169b0b8d1be9c4f190a69c5e4d13159bd8fc
jinhengpang <pangjinheng58@gmail.com>
2025-10-26 11:48:36 +1100

    Add svg class name

--- src/svgs/LinkedinIcon.tsx
+++ src/svgs/LinkedinIcon.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import type { SVGProps } from "react";
 const SvgLinkedinIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
+    className="header-svg"
     xmlns="http://www.w3.org/2000/svg"
     width="3.5em"
     height="2.5em"
