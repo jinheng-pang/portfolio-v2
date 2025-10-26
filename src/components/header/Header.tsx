30b82873717f9d3a22980029c27a1d7f214f422b
jinhengpang <pangjinheng58@gmail.com>
2025-10-26 15:07:05 +1100

    Populate contact me button in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,35 @@
-import React from "react";
-import './header.css'
+import "./header.css";
 import LinkItem from "../link/LinkItem";
 import SvgMailIcon from "@/svgs/MailIcon";
 import SvgGithubIcon from "@/svgs/GithubIcon";
 import SvgLinkedinIcon from "@/svgs/LinkedinIcon";
+import ModalTrigger from "../modal/ModalTrigger";
+import ContactMe from "../modalChildren/ContactMe";
 
 const Header = () => {
-  return <header className="header neumorphic">
-    <div className="header-titles">
-      <h1 className="header-title">Jin Heng Pang</h1>
-      <p className="header-subtitle">Software Engineer</p>
-    </div>
-    <ul className="header-links">
-      <LinkItem href="hi" Icon={SvgMailIcon} label="Contact"  />
-      <LinkItem href="https://github.com/jinheng-pang" Icon={SvgGithubIcon} label="Github"  />
-      <LinkItem href="https://www.linkedin.com/in/jin-heng-pang/" Icon={SvgLinkedinIcon} label="LinkedIn"  />
-    </ul>
-  </header>;
+  return (
+    <header className="header neumorphic">
+      <div className="header-titles">
+        <h1 className="header-title">Jin Heng Pang</h1>
+        <p className="header-subtitle">Software Engineer</p>
+      </div>
+      <ul className="header-links">
+        <ModalTrigger Icon={SvgMailIcon} label="Contact Me">
+          <ContactMe />
+        </ModalTrigger>
+        <LinkItem
+          href="https://github.com/jinheng-pang"
+          Icon={SvgGithubIcon}
+          label="Github"
+        />
+        <LinkItem
+          href="https://www.linkedin.com/in/jin-heng-pang/"
+          Icon={SvgLinkedinIcon}
+          label="LinkedIn"
+        />
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
