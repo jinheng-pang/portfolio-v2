2846c7ba9e423f222e9a490ce72ff3dc9c8fdd03
jinhengpang <pangjinheng58@gmail.com>
2025-10-26 11:49:37 +1100

    Create a resuable modal component

---
+++ src/components/modal/ModalTrigger.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { SVGProps, useState } from "react";
+import Modal from "./Modal";
+
+interface ModalTriggerProps {
+  Icon: React.FC<SVGProps<SVGSVGElement>>;
+  label: string;
+  children: React.ReactNode;
+}
+
+const ModalTrigger = ({ Icon, label, children }: ModalTriggerProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        className="modal-trigger header-link neumorphic-hover"
+        onClick={() => setIsOpen(true)}
+      >
+        <Icon />
+      </button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        {children}
+      </Modal>
+    </>
+  );
+};
+
+export default ModalTrigger;
