fee4a14f24f378496c3e0cce15409f31243dfa51
jinhengpang <pangjinheng58@gmail.com>
2025-10-27 16:34:43 +1100

    Add experience component

---
+++ src/app/sections/experience/Experience.tsx
@@ -0,0 +1,24 @@
+import { experiences } from "@/components/experience/experience";
+import ExperienceItem from "@/components/experience/experienceItem/ExperienceItem";
+import ExperienceCard from "@/components/experience/experienceCard/ExperienceCard";
+import "./experience.css";
+
+const Experience = () => {
+  return (
+    <div className="experience-container">
+      <h2 className="page-heading neumorphic">Experience</h2>
+      <ul className="experience-content">
+        {experiences.map((experience, index) => {
+          const date = `${experience.startDate} - ${experience.endDate}`;
+          return (
+            <ExperienceItem key={experience.id} date={date}>
+              <ExperienceCard {...experience} />
+            </ExperienceItem>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Experience;
