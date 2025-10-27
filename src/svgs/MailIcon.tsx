815508caa5edfc6d4c4e6b262de32809a59856a2
jinhengpang <pangjinheng58@gmail.com>
2025-10-28 07:57:42 +1100

    Rename header svg class names

--- src/svgs/MailIcon.tsx
+++ src/svgs/MailIcon.tsx
@@ -2,7 +2,7 @@
 import type { SVGProps } from "react";
 const SvgMailIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    className="svg"
+    className="header-svg"
     xmlns="http://www.w3.org/2000/svg"
     width="3.5em"
     height="2.5em"
