fe2204326bda3a3c16d3afba0dabf70ffe17d421
jinhengpang <pangjinheng58@gmail.com>
2025-10-27 16:34:12 +1100

    Populate experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "../components/header/Header";
+import Header from "./sections/header/Header";
+import Experience from "./sections/experience/Experience";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
+      <Experience />
     </div>
   );
 }
