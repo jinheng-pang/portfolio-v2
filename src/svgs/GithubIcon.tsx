e8997ca7bb897869ad25e5f356286bef22cd7e5f
jinhengpang <pangjinheng58@gmail.com>
2025-10-28 07:48:19 +1100

    Rename header svgs class name to improve readability

--- src/svgs/GithubIcon.tsx
+++ src/svgs/GithubIcon.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import type { SVGProps } from "react";
 const SvgGithubIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    className="header-svg"
+    className="svg"
     xmlns="http://www.w3.org/2000/svg"
     width="3.5em"
     height="2.5em"
--- src/svgs/MailIcon.tsx
+++ src/svgs/MailIcon.tsx
@@ -2,7 +2,7 @@
 import type { SVGProps } from "react";
 const SvgMailIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    className="header-svg"
+    className="svg"
     xmlns="http://www.w3.org/2000/svg"
     width="3.5em"
     height="2.5em"
