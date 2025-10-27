994de3243233e2c793b961145182d9e35598172a
jinhengpang <pangjinheng58@gmail.com>
2025-10-27 16:36:03 +1100

    Add experience card component

---
+++ src/components/experience/experienceCard/ExperienceCard.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+import { ExperienceProps } from "../experience";
+import './experience-card.css'
+
+const ExperienceCard = (experience: ExperienceProps) => {
+  const { roleTitle, company, description, fact } = experience;
+  const [isFlip, setIsFlip] = useState(false);
+
+  // if (isFlip)
+  //   return (
+  //     <div className="experience-card-container neumorphic-hover" onClick={() => setIsFlip(!isFlip)}>
+  //       <h3 className="experience-card-heading">Fact</h3>
+  //       <p className="experience-card-description">{fact}</p>
+  //     </div>
+  //   );
+
+  return (
+    <div
+      className="experience-card-container neumorphic-hover"
+      onClick={() => setIsFlip(!isFlip)}
+    >
+      <h3 className="experience-card-heading">{roleTitle}</h3>
+      <p className="experience-card-subheading">{company}</p>
+      <p className="experience-card-description">{description}</p>
+    </div>
+  );
+};
+
+export default ExperienceCard;
