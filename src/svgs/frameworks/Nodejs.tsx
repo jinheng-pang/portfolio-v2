16ac4b413018606c645195caa26f1afa0df2528d
jinhengpang <pangjinheng58@gmail.com>
2025-10-29 07:56:41 +1100

    Add svgs for coding langauges and frameworks

---
+++ src/svgs/frameworks/Nodejs.tsx
@@ -0,0 +1,238 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgNodejs = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlSpace="preserve"
+    id="nodejs_svg__Layer_1"
+    x={0}
+    y={0}
+    viewBox="0 0 500 500"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <style>{".nodejs_svg__st0{fill:#689f63}"}</style>
+    <g id="nodejs_svg__XMLID_1_">
+      <path
+        id="nodejs_svg__XMLID_2_"
+        d="M247.9 361.8c-1.2 0-2.4-.3-3.5-.9l-11-6.5c-1.6-.9-.8-1.2-.3-1.4 2.2-.8 2.6-.9 5-2.3.2-.1.6-.1.8.1l8.5 5c.3.2.7.2 1 0l33-19c.3-.2.5-.5.5-.9v-38c0-.4-.2-.7-.5-.9l-32.9-19c-.3-.2-.7-.2-1 0l-32.9 19c-.3.2-.5.5-.5.9v38c0 .4.2.7.5.9l9 5.2c4.9 2.5 7.9-.4 7.9-3.3V301c0-.5.4-.9 1-.9h4.2c.5 0 .9.4.9.9v37.6c0 6.5-3.6 10.3-9.8 10.3-1.9 0-3.4 0-7.6-2.1l-8.6-5c-2.1-1.2-3.5-3.5-3.5-6v-38c0-2.5 1.3-4.8 3.5-6l33-19c2.1-1.2 4.9-1.2 6.9 0l33 19c2.1 1.2 3.5 3.5 3.5 6v38c0 2.5-1.3 4.8-3.5 6l-33 19c-1.2.6-2.4 1-3.6 1"
+        className="nodejs_svg__st0"
+      />
+      <path
+        id="nodejs_svg__XMLID_3_"
+        d="M258.1 335.6c-14.4 0-17.4-6.6-17.4-12.2 0-.5.4-.9 1-.9h4.3c.5 0 .9.3.9.8.6 4.3 2.6 6.5 11.3 6.5 6.9 0 9.9-1.6 9.9-5.3 0-2.1-.8-3.7-11.6-4.8-9-.9-14.6-2.9-14.6-10.1 0-6.7 5.6-10.6 15-10.6 10.6 0 15.8 3.7 16.4 11.5 0 .3-.1.5-.3.7s-.4.3-.7.3H268c-.4 0-.8-.3-.9-.7-1-4.6-3.5-6-10.3-6-7.6 0-8.5 2.6-8.5 4.6 0 2.4 1 3.1 11.3 4.5 10.1 1.3 14.9 3.2 14.9 10.4s-6 11.3-16.4 11.3m40.1-40.3h1.1c.9 0 1.1-.6 1.1-1 0-1-.7-1-1-1h-1.1zm-1.4-3.2h2.5c.8 0 2.5 0 2.5 1.9 0 1.3-.8 1.6-1.3 1.7 1 .1 1 .7 1.2 1.6.1.6.2 1.6.4 1.9h-1.5c0-.3-.3-2.1-.3-2.2-.1-.4-.2-.6-.7-.6h-1.2v2.9H297v-7.2zm-2.9 3.6c0 3 2.4 5.3 5.3 5.3 3 0 5.3-2.4 5.3-5.3 0-3-2.4-5.3-5.3-5.3s-5.3 2.3-5.3 5.3m11.7 0c0 3.5-2.9 6.4-6.4 6.4s-6.4-2.8-6.4-6.4 3-6.4 6.4-6.4c3.4.1 6.4 2.8 6.4 6.4"
+        className="nodejs_svg__st0"
+      />
+      <path
+        id="nodejs_svg__XMLID_9_"
+        d="M145.8 212.9c0-1.5-.8-2.9-2.1-3.7l-34.9-20.1c-.6-.3-1.3-.5-1.9-.6h-.4c-.7 0-1.3.2-1.9.6l-34.9 20.1c-1.3.8-2.1 2.2-2.1 3.7l.1 54.2c0 .8.4 1.5 1 1.8.6.4 1.5.4 2.1 0L91.5 257c1.3-.8 2.1-2.2 2.1-3.7V228c0-1.5.8-2.9 2.1-3.7l8.8-5.1q1.05-.6 2.1-.6c.7 0 1.5.2 2.1.6l8.8 5.1c1.3.8 2.1 2.1 2.1 3.7v25.3c0 1.5.8 2.9 2.1 3.7l20.8 11.9c.7.4 1.5.4 2.1 0s1-1.1 1-1.8zm164.9 28.2c0 .4-.2.7-.5.9l-12 6.9c-.3.2-.7.2-1.1 0l-12-6.9c-.3-.2-.5-.5-.5-.9v-13.9c0-.4.2-.7.5-.9l12-6.9c.3-.2.7-.2 1.1 0l12 6.9c.3.2.5.5.5.9zm3.3-102.6c-.7-.4-1.5-.4-2.1 0s-1 1.1-1 1.8V194c0 .5-.3 1-.7 1.3q-.75.45-1.5 0l-8.8-5c-1.3-.8-2.9-.8-4.2 0l-35 20.2c-1.3.8-2.1 2.1-2.1 3.6v40.4c0 1.5.8 2.9 2.1 3.7l35 20.2c1.3.8 2.9.8 4.2 0l35-20.2c1.3-.8 2.1-2.1 2.1-3.7V153.8c0-1.5-.8-2.9-2.2-3.7zm116.4 88.4c1.3-.8 2.1-2.1 2.1-3.6v-9.8c0-1.5-.8-2.9-2.1-3.7l-34.7-20.2c-1.3-.8-2.9-.8-4.2 0l-35 20.2c-1.3.8-2.1 2.1-2.1 3.7v40.3c0 1.5.8 2.9 2.1 3.7l34.7 19.8c1.3.7 2.8.7 4.1 0l21-11.7c.7-.4 1.1-1.1 1.1-1.8 0-.8-.4-1.5-1.1-1.8l-35.2-20.2c-.7-.4-1.1-1.1-1.1-1.8v-12.7c0-.8.4-1.5 1.1-1.8l10.9-6.3c.7-.4 1.5-.4 2.1 0l10.9 6.3c.7.4 1.1 1.1 1.1 1.8v10c0 .8.4 1.5 1.1 1.8.7.4 1.5.4 2.1 0z"
+        style={{
+          fill: "#404137",
+        }}
+      />
+      <path
+        id="nodejs_svg__XMLID_14_"
+        d="M393 225c.2-.1.6-.1.8 0l6.7 3.9c.3.1.4.4.4.7v7.7c0 .3-.2.6-.4.7l-6.7 3.9c-.3.1-.6.1-.8 0l-6.7-3.9c-.3-.1-.4-.4-.4-.7v-7.7c0-.3.1-.6.4-.7z"
+        className="nodejs_svg__st0"
+      />
+      <g id="nodejs_svg__XMLID_15_">
+        <defs>
+          <path
+            id="nodejs_svg__XMLID_16_"
+            d="M200.1 189.9 165.3 210c-1.3.8-2.1 2.1-2.1 3.6v40.2c0 1.5.8 2.9 2.1 3.6l34.8 20.1c1.3.7 2.9.7 4.2 0l34.8-20.1c1.3-.8 2.1-2.1 2.1-3.6v-40.2c0-1.5-.8-2.9-2.1-3.6l-34.8-20.1q-1.05-.6-2.1-.6c-1.05 0-1.4.2-2.1.6"
+          />
+        </defs>
+        <clipPath id="nodejs_svg__XMLID_39_">
+          <use
+            xlinkHref="#nodejs_svg__XMLID_16_"
+            style={{
+              overflow: "visible",
+            }}
+          />
+        </clipPath>
+        <linearGradient
+          id="nodejs_svg__SVGID_1_"
+          x1={0.377}
+          x2={0.996}
+          y1={-0.047}
+          y2={-0.047}
+          gradientTransform="rotate(116.114 88.848 136.098)scale(184.3927)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop
+            offset={0}
+            style={{
+              stopColor: "#3e863d",
+            }}
+          />
+          <stop
+            offset={0.3}
+            style={{
+              stopColor: "#3e863d",
+            }}
+          />
+          <stop
+            offset={0.5}
+            style={{
+              stopColor: "#55934f",
+            }}
+          />
+          <stop
+            offset={0.8}
+            style={{
+              stopColor: "#5aad45",
+            }}
+          />
+          <stop
+            offset={1}
+            style={{
+              stopColor: "#5aad45",
+            }}
+          />
+        </linearGradient>
+        <path
+          d="m276.3 206.5-98-48L128.1 261l98 48z"
+          style={{
+            clipPath: "url(#nodejs_svg__XMLID_39_)",
+            fill: "url(#nodejs_svg__SVGID_1_)",
+          }}
+        />
+      </g>
+      <g id="nodejs_svg__XMLID_17_">
+        <defs>
+          <path
+            id="nodejs_svg__XMLID_18_"
+            d="M164.1 256.3c.3.4.7.8 1.2 1.1l29.8 17.2 5 2.9c.7.4 1.6.6 2.4.5.3 0 .6-.1.8-.2l36.7-67.1c-.3-.3-.6-.6-1-.8l-22.8-13.1-12-6.9c-.3-.2-.7-.3-1.1-.4z"
+          />
+        </defs>
+        <clipPath id="nodejs_svg__XMLID_40_">
+          <use
+            xlinkHref="#nodejs_svg__XMLID_18_"
+            style={{
+              overflow: "visible",
+            }}
+          />
+        </clipPath>
+        <linearGradient
+          id="nodejs_svg__SVGID_2_"
+          x1={0.05}
+          x2={0.669}
+          y1={0.133}
+          y2={0.133}
+          gradientTransform="rotate(-36.46 451.806 -77.324)scale(183.7799)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop
+            offset={0}
+            style={{
+              stopColor: "#3e863d",
+            }}
+          />
+          <stop
+            offset={0.57}
+            style={{
+              stopColor: "#3e863d",
+            }}
+          />
+          <stop
+            offset={0.72}
+            style={{
+              stopColor: "#619857",
+            }}
+          />
+          <stop
+            offset={1}
+            style={{
+              stopColor: "#76ac64",
+            }}
+          />
+        </linearGradient>
+        <path
+          d="m121.8 220.7 69.1 93.6 91.5-67.5-69.2-93.7z"
+          style={{
+            clipPath: "url(#nodejs_svg__XMLID_40_)",
+            fill: "url(#nodejs_svg__SVGID_2_)",
+          }}
+        />
+      </g>
+      <g id="nodejs_svg__XMLID_19_">
+        <defs>
+          <path
+            id="nodejs_svg__XMLID_20_"
+            d="M201.8 189.3c-.6.1-1.2.2-1.7.5l-34.7 20 37.4 68.1c.5-.1 1-.2 1.5-.5l34.8-20.1c1.1-.6 1.8-1.7 2-2.9l-38.1-65c-.3-.1-.6-.1-.8-.1z"
+          />
+        </defs>
+        <clipPath id="nodejs_svg__XMLID_41_">
+          <use
+            xlinkHref="#nodejs_svg__XMLID_20_"
+            style={{
+              overflow: "visible",
+            }}
+          />
+        </clipPath>
+        <linearGradient
+          id="nodejs_svg__SVGID_3_"
+          x1={0.13}
+          x2={0.749}
+          y1={0.284}
+          y2={0.284}
+          gradientTransform="translate(149.546 198.963)scale(122.2438)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop
+            offset={0}
+            style={{
+              stopColor: "#6bbf47",
+            }}
+          />
+          <stop
+            offset={0.16}
+            style={{
+              stopColor: "#6bbf47",
+            }}
+          />
+          <stop
+            offset={0.38}
+            style={{
+              stopColor: "#79b461",
+            }}
+          />
+          <stop
+            offset={0.47}
+            style={{
+              stopColor: "#75ac64",
+            }}
+          />
+          <stop
+            offset={0.7}
+            style={{
+              stopColor: "#659e5a",
+            }}
+          />
+          <stop
+            offset={0.9}
+            style={{
+              stopColor: "#3e863d",
+            }}
+          />
+          <stop
+            offset={1}
+            style={{
+              stopColor: "#3e863d",
+            }}
+          />
+        </linearGradient>
+        <path
+          d="M165.5 189.3h75.6v88.6h-75.6z"
+          style={{
+            clipPath: "url(#nodejs_svg__XMLID_41_)",
+            fill: "url(#nodejs_svg__SVGID_3_)",
+          }}
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default SvgNodejs;
