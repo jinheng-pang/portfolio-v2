568e749973b061ad41d532fb6c8af1c25ce48558
jinhengpang <pangjinheng58@gmail.com>
2025-10-28 20:00:39 +1100

    Add external link svg

---
+++ src/svgs/ExternalLinkIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgExternalLinkIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 13 13"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.083}
+      d="M9.75 7.042v3.25a1.083 1.083 0 0 1-1.083 1.083H2.708a1.083 1.083 0 0 1-1.083-1.083V4.333A1.083 1.083 0 0 1 2.708 3.25h3.25M8.125 1.625h3.25v3.25M5.417 7.583l5.958-5.958"
+    />
+  </svg>
+);
+export default SvgExternalLinkIcon;
