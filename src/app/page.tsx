3745f49c5f9305d6b7aaf980e9345402840cc4e1
jinhengpang <pangjinheng58@gmail.com>
2025-10-29 21:12:53 +1100

    Clean up imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "../sections/header/Header";
-import Experience from "../sections/experience/Experience";
-import Projects from "../sections/projects/Projects";
-import Skills from "../sections/skills/Skills";
-import Footer from "../sections/footer/Footer";
+
+import { Header, Experience, Projects, Skills, Footer } from "@/sections";
 
 export default function Home() {
   return (
