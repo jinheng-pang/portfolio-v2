3b031a0f42b35fa2ec39c400e2b9d1836e3025d3
jinhengpang <pangjinheng58@gmail.com>
2025-10-29 13:50:59 +1100

    Update the svg for Java and CSS

--- src/svgs/coding-languages/Css.tsx
+++ src/svgs/coding-languages/Css.tsx
@@ -3,70 +3,25 @@ import type { SVGProps } from "react";
 const SvgCss = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    xmlSpace="preserve"
-    id="css_svg__Layer_1"
-    x={0}
-    y={0}
-    viewBox="0 0 500 500"
+    fill="none"
+    viewBox="0 0 100 100"
     width="1em"
     height="1em"
     {...props}
   >
-    <style>{".css_svg__st3{fill:#cecece}"}</style>
-    <g id="css_svg__XMLID_1_">
-      <path
-        id="css_svg__XMLID_11_"
-        d="m145.7 401.1-23.2-260.4h255L354.3 401l-104.5 29z"
-        style={{
-          fill: "#1f72b5",
-        }}
-      />
-      <path
-        id="css_svg__XMLID_10_"
-        d="m250 407.9 84.4-23.4L354.3 162H250z"
-        style={{
-          fill: "#51a3dc",
-        }}
-      />
-      <path
-        id="css_svg__XMLID_35_"
-        d="M167.1 222.1 164 192h172l-2 34.9-72.7 32.1H331l-7.5 99-70.3 18.9-72.9-17.7-4.5-50.2H208l1.8 26.5 42.2 7.9 39-12 1.8-42.4H173l-2.2-31.5 79.2-34.7z"
-        style={{
-          fill: "#ebebeb",
-        }}
-      />
-      <g id="css_svg__XMLID_2_">
-        <path
-          id="css_svg__XMLID_39_"
-          d="M170.8 257.5 173 289h77v-66.2z"
-          className="css_svg__st3"
-        />
-        <path
-          id="css_svg__XMLID_40_"
-          d="M209.8 335.5 208 309h-32.2l4.5 50.2 69.7 16.9V343z"
-          className="css_svg__st3"
-        />
-        <path
-          id="css_svg__XMLID_41_"
-          d="m164 192 3.1 30.1 82.9.7V192z"
-          className="css_svg__st3"
-        />
-      </g>
-      <g id="css_svg__XMLID_36_">
-        <path
-          id="css_svg__XMLID_34_"
-          d="M274 82V70h-48v28h33v9h-34v11h48V89h-34v-7z"
-        />
-        <path
-          id="css_svg__XMLID_38_"
-          d="M187 98.3v8.7h34v11h-48V70h47v12h-33z"
-        />
-        <path
-          id="css_svg__XMLID_37_"
-          d="M327 82V70h-48v28h33v9h-34v11h48V89h-34v-7z"
-        />
-      </g>
-    </g>
+    <path
+      fill="#264DE4"
+      d="m94.175 0-8.033 89.99L50.034 100l-36.01-9.996L6 0z"
+    />
+    <path fill="#2965F1" d="m79.265 84.26 6.864-76.9H50.087v84.989z" />
+    <path
+      fill="#EBEBEB"
+      d="m24.396 40.74.99 11.04h24.702V40.74zm25.692-22.342h-27.68l1.003 11.039h26.676zm-.001 62.496V69.409l-.048.013-12.294-3.32-.786-8.804H25.878l1.547 17.332 22.612 6.278z"
+    />
+    <path
+      fill="#fff"
+      d="m63.642 51.779-1.281 14.316-12.312 3.323v11.484l22.63-6.271.166-1.865 2.594-29.061.27-2.964L77.7 18.398H50.05v11.039h15.555L64.599 40.74H50.05v11.038z"
+    />
   </svg>
 );
 export default SvgCss;
