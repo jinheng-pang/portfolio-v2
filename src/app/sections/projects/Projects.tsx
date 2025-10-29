57a0489b2f81e4c76c2e0811b83d8c7ab31cc330
jinhengpang <pangjinheng58@gmail.com>
2025-10-29 14:09:50 +1100

    Initialise project cards

--- src/app/sections/projects/Projects.tsx
+++ src/app/sections/projects/Projects.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { projects } from "@/components/project/project";
+import ProjectCard from "@/components/project/projectCard/ProjectCard";
 
 const Projects = () => {
   return (
-    <div className='projects page-section'>
-        <h2 className='page-subheading neumorphic'>Projects</h2>
+    <div className="projects page-section">
+      <h2 className="page-subheading neumorphic">Projects</h2>
+      {projects.map((project) => (
+        <ProjectCard key={project.title} {...project} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
