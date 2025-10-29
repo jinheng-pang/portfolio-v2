fd2121a5f8a79ed1ddd998aef7ce6c89d684fcb0
jinhengpang <pangjinheng58@gmail.com>
2025-10-29 21:13:54 +1100

    Add title and remove pointy cursor for SvgLink component

--- src/components/link/SvgLink/SvgLink.tsx
+++ src/components/link/SvgLink/SvgLink.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import './svg-link.css'
 
 interface SvgLinkProps {
   href: string;
   Icon: React.FC<React.SVGProps<SVGSVGElement>>;
   className?: string;
+  title?: string; 
 }
 
-const SvgLink: React.FC<SvgLinkProps> = ({ href, Icon, className = "" }) => {
+const SvgLink: React.FC<SvgLinkProps> = ({ href, Icon, className = "", title }) => {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
       className={`svg-link ${className}`}
+      title={title}
     >
       <Icon className="svg-icon" />
     </a>
