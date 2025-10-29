244b0264e922d433db145a1cd9fce184e3b3cf07
jinhengpang <pangjinheng58@gmail.com>
2025-10-29 21:13:02 +1100

    Clean up exports

---
+++ src/sections/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./header/Header";
+export { default as Experience } from "./experience/Experience";
+export { default as Projects } from "./projects/Projects";
+export { default as Skills } from "./skills/Skills";
+export { default as Footer } from "./footer/Footer";
